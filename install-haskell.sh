ca6e5f0548b0fbdaf31643f6731956f61436d752
Bryan Gardiner <bog@khumba.net>
2015-04-15 22:49:21 -0700

    Remove lang/hs/ hierarchy and special build and install scripts.
    There's no C++ runtime support with Cppop now, so there's no need for
    these.

--- install-haskell.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-. "$(dirname "$(realpath "$0")")/common.sh"
-
-echo
-msg "Installing the generator and Haskell bindings."
-run cabal install "$projectDir/lang/hs"
