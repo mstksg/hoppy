00338ddda7a6baa1cfe67bfd67a5261e52f2592e
Bryan Gardiner <bog@khumba.net>
2017-05-25 23:17:07 -0700

    Version bump to hoppy-generator-0.3.3.

--- generator/default.nix
+++ generator/default.nix
@@ -22,7 +22,7 @@
 }:
 mkDerivation ({
   pname = "hoppy-generator";
-  version = "0.3.2";
+  version = "0.3.3";
   src = ./.;
   libraryHaskellDepends = [
     base containers directory filepath haskell-src mtl
