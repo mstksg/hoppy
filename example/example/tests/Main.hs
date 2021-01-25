e1c3d7ce8f0bdffa8c06c3a970066ccfeb8584b0
Bryan Gardiner <bog@khumba.net>
2021-01-25 06:03:23 -0800

    Update copyrights for 2021.

--- example/example/tests/Main.hs
+++ example/example/tests/Main.hs
@@ -1,6 +1,6 @@
 -- This file is part of Hoppy.
 --
--- Copyright 2015-2020 Bryan Gardiner <bog@khumba.net>
+-- Copyright 2015-2021 Bryan Gardiner <bog@khumba.net>
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,7 +2,7 @@
 
 # This file is part of Hoppy.
 #
-# Copyright 2015-2020 Bryan Gardiner <bog@khumba.net>
+# Copyright 2015-2021 Bryan Gardiner <bog@khumba.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
